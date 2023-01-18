6db7593611d57118b25d8d9195300fb4e2f6f106
sushthecoda <coolsushaan@gmail.com>
2023-01-18 11:04:14 +0530

    contract additions

---
+++ nftlicense.sol
@@ -0,0 +1,31 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("MyNFT", "NFT") {}
+    
+    mapping (string => string) details;
+    function mintNFT(address recipient,string memory params,string memory uuid, string memory tokenURI)
+        public onlyOwner
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+         details[uuid] = params;
+        
+    }
+    function fetchbyuuid(string memory uuid) public view onlyOwner returns (string memory){
+     return  details[uuid];
+    }
+}
